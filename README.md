3a6f10ab22b7805e8c0eef7116435e535f5e0e9d
Bharadwaj Chukkala <bchukkal@terpmail.umd.edu>
2023-11-11 15:58:05 +0000

    Adding Calculator

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DSA-Python
+
+# List of programs
+
+- Adding two numbers
+- Subtracting two numbers
+- Calculator with basic functionalities
---
+++ calculator.py
@@ -0,0 +1,10 @@
+'''
+Use object oriented programming to create a calculator
+that can add, subtract, multiply, divide, and find the square root of numbers
+It should be able to perform all of these functions both on a single number or between two numbers.
+'''
+import math
+import numpy as np
+
+
+class Calculator:
